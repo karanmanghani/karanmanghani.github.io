2f72338e53345c17f4b0545a9a3e90eef547745c
karanmanghani <karanmanghani14@gmail.com>
2020-08-04 16:11:40 -0400

    Updates

---
+++ precache-manifest.db380c892e532b9e5b9154e8f2b54e1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fe8ac5dee36652fe55b3f384e8142b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aadd12d42cfc35d6a653",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "061e7d87237e6e858441",
+    "url": "/static/js/2.b2a8cf0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b2a8cf0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aadd12d42cfc35d6a653",
+    "url": "/static/js/main.c3aee5cf.chunk.js"
+  },
+  {
+    "revision": "8daaee6d59e2570e8959",
+    "url": "/static/js/runtime-main.005337c9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
